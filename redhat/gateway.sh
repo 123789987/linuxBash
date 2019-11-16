bd359241ecc236ba6a6b6b1902076822d4d0088f
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-16 00:31:34 -0800

    Update gateway.sh

--- redhat/gateway.sh
+++ redhat/gateway.sh
@@ -1,12 +1,6 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sysctl -p
-route add 0.0.0.0/0 gw 192.168.122.1
 route del default
-route del 0.0.0.0/0 gw 192.168.122.1
-route add 0.0.0.0 netmask 0.0.0.0 gw 192.168.122.1
-route del 0.0.0.0 netmask 0.0.0.0 gw 192.168.122.1
-route add default gw 192.168.122.1
-route del default gw 192.168.122.1
 route add default gw 192.168.100.154
 route add default gw 192.168.122.1 dev ens3
 systemctl enable firewalld
