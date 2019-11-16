22c38783d897a96a9c07ce857572e59e9c5840cc
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-15 22:59:36 -0800

    Update init.sh

--- redhat/init.sh
+++ redhat/init.sh
@@ -1,2 +1,5 @@
+cd
 yum install wget -y
+wget https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-7.el8.noarch.rpm
+rpm -Uvh epel-release-8-7.el8.noarch.rpm
 yum update -y
