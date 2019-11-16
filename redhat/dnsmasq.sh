4f2265d4fcf6d766fc24582d83b3e032f85c636c
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-15 23:44:28 -0800

    Update dnsmasq.sh

--- redhat/dnsmasq.sh
+++ redhat/dnsmasq.sh
@@ -1,11 +1,11 @@
 cd
 yum install dnsmasq -y
-cat 'resolv-file=/etc/resolv.dnsmasq.conf' >> /etc/dnsmasq.conf
-cat 'strict-order' >> /etc/dnsmasq.conf
-cat 'listen-address=10.8.0.1' >> /etc/dnsmasq.conf
-cat 'addn-hosts=/etc/addion_hosts' >> /etc/dnsmasq.conf
-cat 'nameserver 8.8.8.8' >> /etc/resolv.dnsmasq.conf
-cat 'nameserver 8.8.8.8' >> /etc/resolv.conf
-cat '127.0.0.1 localhost' >> /etc/addion_hosts
+echo 'resolv-file=/etc/resolv.dnsmasq.conf' >> /etc/dnsmasq.conf
+echo 'strict-order' >> /etc/dnsmasq.conf
+echo 'listen-address=10.8.0.1' >> /etc/dnsmasq.conf
+echo 'addn-hosts=/etc/addion_hosts' >> /etc/dnsmasq.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.dnsmasq.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo '127.0.0.1 localhost' >> /etc/addion_hosts
 service dnsmasq start 
 chkconfig dnsmasq on
