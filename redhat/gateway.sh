f510eb99dc16ac76dbcad51e15bdefbd08507afc
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-16 00:59:22 -0800

    Delete gateway.sh

--- redhat/gateway.sh
+++
@@ -1,9 +0,0 @@
-# edit: dev
-echo 1 > /proc/sys/net/ipv4/ip_forward
-systemctl enable firewalld
-systemctl start firewalld
-firewall-cmd --permanent --zone=public --add-masquerade
-firewall-cmd --direct --permanent --add-rule ipv4 filter INPUT 0 --in-interface ens3 --protocol vrrp                                                                                                                        
-firewall-cmd --reload
-echo '192.168.100.0/24 via 192.168.122.1' > /etc/sysconfig/network-scripts/route-ens4
-nmcli con reload
