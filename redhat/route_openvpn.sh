b75afbdcb98faf0d12f6968f934b7aea52518c35
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-16 01:27:38 -0800

    Update route_openvpn.sh

--- redhat/route_openvpn.sh
+++ redhat/route_openvpn.sh
@@ -1,4 +1,4 @@
-# edit: dev
+# edit: client.conf
 yum install wget -y
 wget https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-7.el8.noarch.rpm
 rpm -Uvh epel-release-8-7.el8.noarch.rpm
