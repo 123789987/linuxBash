0faa0be252194d7e7051a295ee7241f2fe453122
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-18 02:18:28 +0000

    Update init.sh

--- redhat/init.sh
+++ redhat/init.sh
@@ -3,3 +3,8 @@ yum install wget -y
 wget https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-7.el8.noarch.rpm
 rpm -Uvh epel-release-8-7.el8.noarch.rpm
 yum update -y
+setenforce 0
+vi /etc/selinux/config
+echo '1' > /proc/sys/net/ipv4/ip_forward
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+sysctl -p
