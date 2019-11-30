37a40ecf26c8497ef585827e39583d0769fe7e6c
123789987 <49760022+123789987@users.noreply.github.com>
2019-11-30 17:05:54 +0000

    Update init.sh

--- redhat/init.sh
+++ redhat/init.sh
@@ -1,10 +1,12 @@
 cd
 yum install wget -y
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-rpm -Uvh epel-release-8-7.el8.noarch.rpm
+rpm -ivh epel-release-8-7.el8.noarch.rpm
 yum update -y
 setenforce 0
 vi /etc/selinux/config
 echo '1' > /proc/sys/net/ipv4/ip_forward
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.disable_ipv6=1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6=1' >> /etc/sysctl.conf
 sysctl -p
